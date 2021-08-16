1682c3198ccf64a92fc91d75feff3c8f77f36287
CagionSource <78655390+CagionSource@users.noreply.github.com>
2021-08-16 19:27:31 +0700

    add backend registrasi_pasien
    Yang perluh di perbaiki 
    Icon Field, Text  Validator dan enkripsi password

---
+++ lib/services/services.dart
@@ -0,0 +1,7 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'database.dart';
